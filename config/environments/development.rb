fb7ca96c7b1392cf2bd967eced8e4e40044dfd42
laemdfk <hashimo0225@gmail.com>
2022-06-04 17:27:14 +0000

    [Add]devise実装,admin/public controller,view 分けて実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "940fc72e391c45e0b8017fa9b70c4415.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
