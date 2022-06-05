bf8c162e4875b92ba7e68fd629d99e5cf6c2cd9f
laemdfk <hashimo0225@gmail.com>
2022-06-05 12:10:39 +0000

    [Update]ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/destroy'
-  end
-  namespace :admin do
-    get 'endusers/index'
-    get 'endusers/show'
-    get 'endusers/edit'
-    get 'endusers/update'
-    get 'endusers/destroy'
-  end
+
   devise_for :end_users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -19,5 +8,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    root to: 'endusers#index'
+    resources :endusers, only:[:index, :show, :edit, :update, :destroy]
+    resources :posts, only:[:index, :show, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
