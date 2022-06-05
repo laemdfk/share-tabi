3e645edb8466316bd256007bf5f1d4aeb8c1ae14
laemdfk <hashimo0225@gmail.com>
2022-06-05 13:14:37 +0000

    [Update]ルーティング修正,view雛形作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,10 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    # @post = Post.find(params[:id])
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'endusers#index'
 
-    resources :endusers, only:[:index, :show, :edit, :update, :destroy]
+    resources :endusers, only:[:show, :edit, :update, :destroy]
     resources :posts, only:[:index, :show, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
