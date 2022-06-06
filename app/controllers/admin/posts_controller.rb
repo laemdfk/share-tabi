991ceaa1eef3b0911ee57e23f88cea30b17bb90b
laemdfk <hashimo0225@gmail.com>
2022-06-06 08:59:15 +0000

    [Add]admin/posts view雛形作成,posts controller記述

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,9 +4,13 @@ class Admin::PostsController < ApplicationController
   end
 
   def show
-    # @post = Post.find(params[:id])
+     @post = Post.find(params[:id])
+     @enduser = @post.enduser
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @Post.destroy
+    redirect_to posts_path
   end
 end
