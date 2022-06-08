f71dcd2315b4f1b59c63ad70d4cecf2c1df7dd57
laemdfk <hashimo0225@gmail.com>
2022-06-08 09:22:51 +0000

    [Add]root_pathへのリンク設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+# 設定すると、admin側でログインできなくなるためコメントアウト
+
+# before_action :authenticate_end_user!, except: [:top]
+# before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+#   protected
+
+# #config側で「名前認証」の設定をしたため、ここでemailを許可
+# def configure_permitted_parameters
+#   devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+# end
 end
