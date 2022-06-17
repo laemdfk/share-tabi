00143ce3ed8d97f6b4f7051b660b03841da8e55e
laemdfk <kappa021618@icloud.com>
2022-06-17 12:59:26 +0000

    [Update]posts#show view修正/favorite#controller destroy_action修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,17 @@
 class Public::FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_end_user.favorites.new(post_id: post.id)
-    favorite.save
-    redirect_to public_posts_path
-  end
+  post = Post.find(params[:post_id])
+  favorite = current_end_user.favorites.new(post_id: post.id)
+  favorite.save
+  redirect_to public_post_path(post)
+ end
 
-  def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_end_user.favorites.find_by(post_id: post.id)
-    favorite.save
-    redirect_to public_posts_path
-  end
+
+ def destroy
+  post = Post.find(params[:post_id])
+  favorite = current_end_user.favorites.find_by(post_id: post.id)
+  favorite.destroy   # 内容を破棄する必要があるので「destroy」。saveにしていたので発火しなかった
+  redirect_to public_post_path(post)
+ end
 end
+
